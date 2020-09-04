fd1826210b54c7c966b47b09fdbf690ec99303fc
bangnguyen1995 <56287801+bangnguyen1995@users.noreply.github.com>
2020-09-04 12:26:33 +0700

    updatedilejs

--- src/main/resources/static/js/main3.js
+++ src/main/resources/static/js/main3.js
@@ -1,11 +1,11 @@
 /**
  * 
  */
-$(document).ready(function () {
-	$('.table .delbtn').on('click',function(event){
-		event.preventDefault();
-		var href = $(this).attr('href');
-		$('.myform #delRef').attr('href',href);
-		$('.myform #exampleModal').modal();
-	});
-});
+	$(document).ready(function() {
+					$('.table #delbtn').on('click', function(event) {
+						event.preventDefault();
+						var href = $(this).attr('href');
+						$('.myform #btnhref').attr('href', href);
+						$('.myform #exampleModal').modal();
+					});
+				});
